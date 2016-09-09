f7ed45b117101730cbfa7a5e69449b796fee375b
Jason Wong & Angel Chen <pair+JWong411.angel-chen@devbootcamp.com>
2016-09-09 15:12:41 -0400

    Finish working MVP

--- controller.rb
+++ controller.rb
@@ -1,6 +1,8 @@
 require_relative 'view'
-require_relative 'model'
-
+require_relative 'complaint_result'
+require_relative 'parser'
+require_relative 'complaint'
+require 'pry'
 
 class Controller
   attr_reader :view, :complaints
@@ -13,19 +15,22 @@ class Controller
 
   def run
     view.welcome
-    view.prompt_user_for_zip
-    input = view.get_user_input
-    results = Parser.parse_by(input)
-    results.complaints.each do |result|
-     view.display(result.created_date)
-     view.display(result.complaint_type)
-     view.display(result.descriptor)
-     view.display(result.incident_address)
-     view.display(result.incident_zip)
-     view.display(result.city)
-     view.display(result.status)
-     view.separator
-   end
-  end
+    input = nil
+    until input == "exit"
+      view.prompt_user_for_zip
+      input = view.get_user_input
+        results = Parser.parse_by(input)
+        results.complaints.each do |result|
+          view.display(result.created_date)
+          view.display(result.complaint_type)
+          view.display(result.descriptor)
+          view.display(result.incident_address)
+          view.display(result.incident_zip)
+          view.display(result.city)
+          view.display(result.status)
+          view.separator
+          end
+      end
 
+  end
 end
