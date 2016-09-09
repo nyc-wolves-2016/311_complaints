1df065c8bd3d02c7940cd5f9b3e2ce21a027912e
Jason Wong & Angel Chen <pair+JWong411.angel-chen@devbootcamp.com>
2016-09-09 13:56:10 -0400

    Add runner and modified controller/view

--- controller.rb
+++ controller.rb
@@ -8,17 +8,24 @@ class Controller
   def initialize(args={})
     @view = args.fetch(:view)
     @complaints = args.fetch(:complaints)
+    run
   end
 
   def run
+    view.welcome
     view.prompt_user_for_zip
-    view.get_user_input
-    
-
-
-
-
-
+    input = view.get_user_input
+    results = Parser.parse_by(input)
+    results.complaints.each do |result|
+     view.display(result.created_date)
+     view.display(result.complaint_type)
+     view.display(result.descriptor)
+     view.display(result.incident_address)
+     view.display(result.incident_zip)
+     view.display(result.city)
+     view.display(result.status)
+     view.separator
+   end
   end
 
 end
