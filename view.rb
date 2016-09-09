2b8f994a5371bf918445b8353a45f49f6f065f7e
Jason Wong & Angel Chen <pair+JWong411.angel-chen@devbootcamp.com>
2016-09-09 11:52:19 -0400

    Add initial controller setup

--- view.rb
+++ view.rb
@@ -4,18 +4,20 @@ module View
     
   end
 
-  def get_user_input
+  def prompt_user_for_zip
     puts "Please input a zip code to search for 311 complaints."
+  end
+
+  def prompt_user_for_street_name
+    puts "Would you like to search for incidents on your street?"
+  end
+
+  def get_user_input
     STDIN.gets.chomp
   end
 
   def display(result)
     puts "#{result}"
-  end
-
-  def get_user_street_name
-    puts "Would you like to search for incidents on your street?"
-    STDIN.gets.chomp
   end
 
 end
